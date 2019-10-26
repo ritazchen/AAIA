a859c906ba93d4f0935552e12b5997feb6b3dfbf
veto14 <42426906+veto14@users.noreply.github.com>
2019-10-26 20:26:59 -0300

    Atualizações com regras de movimentação, apesar da movimentação em si ainda não ter sido implementada.

--- jogovelha.c
+++ jogovelha.c
@@ -14,23 +14,139 @@ typedef struct tabuleiro{
 	Peca** board; //estrutura do tabuleiro
 }Tabuleiro;
 
-Tabuleiro* inicializaTabuleiro(); //mistura algumas funções pra gerar o tabuleiro inicial
+Tabuleiro* inicializaTabuleiro(); //mistura algumas funcoes pra gerar o tabuleiro inicial
 Peca getPeca(Tabuleiro* t,int x, int y); //troca a peca no tabuleiro
-Peca recebeInformacao(Peca info); //passa tudo de uma estrutura peça pra outra
+Peca recebeInformacao(Peca info); //passa tudo de uma estrutura peca pra outra
 void moveTabuleiro(Tabuleiro* t, Peca x, Peca y);
 void liberaTabuleiro(Tabuleiro* t); //free coletivo
 void mostraTabuleiro(Tabuleiro* t); //printa o tabuleiro
 void poePeca(Tabuleiro* t,int x, int y, int id); //id vai ser 1 pra branco, -1 pra preto.
-void movePeca(Tabuleiro* t, Peca p, int xDestino, int yDestino); //troca as informa��es da pe�a
+void movePeca(Tabuleiro* t, Peca p, int xDestino, int yDestino); //troca as informacoes da peca
+void clr(); //limpa tela
 char getCharPeca(Peca p); //retorna um caracter da peca desejada (REPRESENTACAO VISUAL)
 int setCor(Peca p); //gera uma string da cor da peca
-void DEBUG_printaPeca(Peca p); //fun��o pra entregar os specs da pe�a //todas as fun��es debug v�o ser deletadas depois
+int getID(Peca p); //pega a ID da cor da peca
+int isCome(Tabuleiro* t, Peca p, Peca alvo); //verifica se é um movimento normal, caso seja retorna 0, ou se é um movimento que come outra peca, retornando 1, caso seja invalido, retorna -1.
+int direcao(Peca p, Peca alvo); //funcao pra ver se o alvo ta a esquerda ou direita da peca original (-1 esquerda, 1 direita)
+int atrasVazio(Tabuleiro* t, Peca p,Peca alvo, int dir); //retorna 1 se a casa atras do alvo ta vazia, 0 se tem alguem, -1 se a casa de tras nao existir
+int isVencedor(Tabuleiro *t); //retorna 1 se brancas vencem, -1 se pretas vencem, 0 se ninguem venceu
+int isPeca(Peca p); //verifica se e uma peca ou um espaco (1 se peca, 0 espaco)
+int getX(Peca p); //retorna X (linha que a peca ta)
+int getY(Peca p); //retorna Y (coluna q a peca ta)
+void DEBUG_printaPeca(Peca p); //funcao pra entregar os specs da peca //todas as funcoes debug vao ser deletadas depois
 
 int main (void){
 	Tabuleiro* tab;
+	int finaliza = 0;
 	tab = inicializaTabuleiro();
-	mostraTabuleiro(tab);
+	while(isVencedor(tab) == 0 && finaliza != 42){ //game loop (remover a variavel finaliza [unicamente pra parar o while quando eu quiser])
+		mostraTabuleiro(tab);
+		finaliza = 42;
+	}
 	liberaTabuleiro(tab);
+}
+
+int isCome(Tabuleiro* t,Peca p, Peca alvo){
+	if(getID(p) != getID(alvo)){//verifica se uma peca nao ta tentando mover contra outra do mesmo tipo
+		if(getID(p) == 0 || getID(alvo) == 0){ //cai nesse laço se alguma casa (ou p, ou alvo) for vazia.
+			return 0;
+		}
+		else if(getID(p) + getID(alvo) == 0){ //caso seja um movimento de comer valido, a soma do ID de uma peça preta -1 e de uma branca 1 sempre vai dar zero
+			if(atrasVazio(t,p,alvo,direcao(p,alvo)) == 1){
+				return 1;
+			}
+		}
+		else{
+			return -1;
+		}
+	}
+}
+
+int atrasVazio(Tabuleiro* t, Peca p, Peca alvo, int dir){ 
+	int auxX = getX(alvo);
+	int auxY = getY(alvo);
+	if(getX(p) > auxX){ //caso caia aqui, p esta abaixo do alvo
+		if(dir == -1){// caso esquerda
+			if(auxX != 0 && auxY != 0){ //verifica se a casa acima a esquerda existe
+				auxX--;
+				auxY--;
+				if(isPeca(t->board[auxX][auxY]) == 0){
+					return 1;
+				}
+				else{
+					return 0;
+				}
+			}
+			else{
+				return -1;
+			}
+		}
+		if(dir == 1){//caso direita
+			if(auxX != 0 && auxY != 7){ //verifica se a casa acima a direita existe
+				auxX--;
+				auxY++;
+				if(isPeca(t->board[auxX][auxY]) == 0){
+					return 1;
+				}
+				else{
+					return 0;
+				}	
+			}
+			else{
+				return -1;
+			}
+		}
+	}
+	else{ //caso caia aqui, p esta acima do alvo
+		if(dir == -1){// caso esquerda
+			if(auxX != 7 && auxY != 0){ //verifica se a casa abaixo a esquerda existe
+				auxX++;
+				auxY--;
+				if(isPeca(t->board[auxX][auxY]) == 0){
+					return 1;
+				}
+				else{
+					return 0;
+				}	
+			}
+			else{
+				return -1;
+			}
+		}
+		else{ //caso direita
+			if(auxX != 7 && auxY != 7){ //verifica se a casa abaixo a direita existe
+				auxX++;
+				auxY++;
+				if(isPeca(t->board[auxX][auxY]) == 0){
+					return 1;
+				}
+				else{
+					return 0;
+				}	
+			}
+			else{
+				return -1;
+			}
+		}
+	}
+}
+
+int isPeca(Peca p){
+	if(p.cor == 1 || p.cor == -1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int direcao(Peca p, Peca alvo){
+	if(getY(p) > getY(alvo)){
+		return -1;
+	}
+	else{
+		return 1;
+	}
 }
 
 void movePeca(Tabuleiro* t, Peca p, int xDestino, int yDestino){
@@ -79,6 +195,18 @@ void mostraTabuleiro(Tabuleiro* t){ //deixar mais semioticamente viavel
 	}
 }
 
+int getX(Peca p){
+	return p.x;
+}
+
+int getY(Peca p){
+	return p.y;
+}
+
+int getID(Peca p){
+	return p.cor;
+}
+
 char getCharPeca(Peca p){
 	if(p.cor == 1){
 		return 'b';
@@ -88,6 +216,31 @@ char getCharPeca(Peca p){
 	}
 	else{
 		return ' ';
+	}
+}
+
+int isVencedor(Tabuleiro *t){
+	int i, j;
+	int temBrancas = 0;
+	int temPretas = 0;
+	for(i = 0; i < tam; i++){
+		for(j = 0; j < tam; j++){
+			if(getID(t->board[i][j]) == 1){
+				temBrancas = 1;
+			}
+			else if(getID(t->board[i][j]) == -1){
+				temPretas = 1;
+			}
+		}
+	}
+	if(temBrancas == 0){
+		return -1; //pretas vencem
+	}
+	else if(temPretas == 0){
+		return 1; //brancas vencem
+	}
+	else{
+		return 0; //jogo ainda não acabou
 	}
 }
 
@@ -142,4 +295,8 @@ void poePeca(Tabuleiro* t,int x, int y, int id){
 void liberaTabuleiro(Tabuleiro* t){
 	free(t->board);
 	free(t);
+}
+
+void clr(){
+	system("@cls||clear");
 }
