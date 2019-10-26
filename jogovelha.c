c53b6bcab305c8db42ef08215a56a104a296bac8
veto14 <42426906+veto14@users.noreply.github.com>
2019-10-26 15:49:45 -0300

    Update com funções de movimento

--- jogovelha.c
+++ jogovelha.c
@@ -4,28 +4,64 @@
 #define tam 8
 
 typedef struct peca{
-	int cor;
-	char corString[10];
-	int x;
-	int y;
+	int cor; //codigo de cor
+	char corString[10]; //nome da cor (semiotica)
+	int x; //coordenada horizontal
+	int y; //coordenada vertical
 }Peca;
 
 typedef struct tabuleiro{
-	Peca** board;
+	Peca** board; //estrutura do tabuleiro
 }Tabuleiro;
 
-Tabuleiro* inicializaTabuleiro();
-void liberaTabuleiro(Tabuleiro* t);
-void mostraTabuleiro(Tabuleiro* t);
+Tabuleiro* inicializaTabuleiro(); //mistura algumas funções pra gerar o tabuleiro inicial
+Peca getPeca(Tabuleiro* t,int x, int y); //troca a peca no tabuleiro
+Peca recebeInformacao(Peca info); //passa tudo de uma estrutura peça pra outra
+void moveTabuleiro(Tabuleiro* t, Peca x, Peca y);
+void liberaTabuleiro(Tabuleiro* t); //free coletivo
+void mostraTabuleiro(Tabuleiro* t); //printa o tabuleiro
 void poePeca(Tabuleiro* t,int x, int y, int id); //id vai ser 1 pra branco, -1 pra preto.
+void movePeca(Tabuleiro* t, Peca p, int xDestino, int yDestino); //troca as informa��es da pe�a
 char getCharPeca(Peca p); //retorna um caracter da peca desejada (REPRESENTACAO VISUAL)
 int setCor(Peca p); //gera uma string da cor da peca
+void DEBUG_printaPeca(Peca p); //fun��o pra entregar os specs da pe�a //todas as fun��es debug v�o ser deletadas depois
 
 int main (void){
 	Tabuleiro* tab;
 	tab = inicializaTabuleiro();
 	mostraTabuleiro(tab);
 	liberaTabuleiro(tab);
+}
+
+void movePeca(Tabuleiro* t, Peca p, int xDestino, int yDestino){
+	char auxString[10];
+	int auxX, auxY, auxCor;
+	Peca aux, destino;
+	destino = getPeca(t,xDestino,yDestino);
+	moveTabuleiro(t,p,destino);
+}
+
+void DEBUG_printaPeca(Peca p){
+	printf("Par Ordenado Cartesiano(x,y) = (%d,%d) Cor = %d\n",p.x,p.y,p.cor);
+}
+
+void moveTabuleiro(Tabuleiro* t,Peca x, Peca y){
+	Peca aux = recebeInformacao(y);
+	t->board[y.x][y.y] = recebeInformacao(t->board[x.x][x.y]);
+	t->board[x.x][x.y] = recebeInformacao(aux);
+}
+
+Peca recebeInformacao(Peca info){
+	Peca destino;
+	destino.cor = info.cor;
+	setCor(destino);
+	destino.x = info.x;
+	destino.y = info.y;
+	return destino;
+}
+
+Peca getPeca(Tabuleiro* t, int x, int y){
+	return t->board[x][y];
 }
 
 void mostraTabuleiro(Tabuleiro* t){ //deixar mais semioticamente viavel
@@ -63,7 +99,7 @@ int setCor(Peca p){
 		strcpy(p.corString,"Preto");
 	}
 	if(p.cor == 0){
-		strcpy(p.corString," ");
+		strcpy(p.corString,"  ");
 	}
 }
 
